675460086b9f488655f3324095f085efb0cda666
shobhith_k <54988109+shobhithk@users.noreply.github.com>
2022-08-03 09:38:01 +0530

    Delete tinyfsck-main directory

--- tinyfsck-main/src/exitcode.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define TF_SUCCESS              0
-#define TF_INCORRECT_USAGE      1
-#define TF_NOT_EXT2             2
-#define TF_FS_MOUNTED           3
-#define TF_CRITCAL_ERROR        4
--- tinyfsck-main/src/utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-std::string get_fs_type(std::string& path);
-bool is_fs_mounted(std::string& path, std::string& mount_point);
